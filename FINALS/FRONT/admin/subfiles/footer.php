1afcd54d609866ba07e1f886633ed7804d631c0d
MARK DELGADO RIÑON <112269719+mdrinon@users.noreply.github.com>
2022-12-19 16:23:32 -0800

    Create footer.php

---
+++ FINALS/FRONT/admin/subfiles/footer.php
@@ -0,0 +1,10 @@
+        <!--footer section--->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">All rights reserved.<br> Developed By <b><a href="../index.php"> Martha's Group </a></b></p>
+            </div>
+        </div>
+        <!---end of footer-->
+
+    </body>
+</html>
